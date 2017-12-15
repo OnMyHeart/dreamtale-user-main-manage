5cab3973a65640adedbf2b5351a8d7bd1b380f14
yingshuai <yingshuai>
2017-12-15 16:08:21 +0800

    base

--- dreamtale-user-service/src/main/java/com/dreamtale/user/service/UserServiceApp.java
+++ dreamtale-user-service/src/main/java/com/dreamtale/user/service/UserServiceApp.java
@@ -2,8 +2,15 @@ package com.dreamtale.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+// 组件扫描。需要扫描加载common等其他依赖包中的组件
+@ComponentScan(basePackages = { "com.dreamtale" })
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@ServletComponentScan(basePackages = { "com.dreamtale" })
 public class UserServiceApp {
 
 	public static void main(String[] args) {
