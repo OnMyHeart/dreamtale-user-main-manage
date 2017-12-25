527f7732da664b5eed7f32e565592b2010bd7ba6
yingshuai <865887234@qq.com>
2017-12-25 22:14:56 +0800

    mybatis集成01

---
+++ dreamtale-user-service/src/main/java/com/dreamtale/user/service/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.dreamtale.user.service.dao;
+
+/**
+ * Created by yingshuai on 2017/12/24.
+ */
+public interface UserDao {
+}
--- dreamtale-user-service/src/main/java/com/dreamtale/user/service/service/UserRegisterService.java
+++ dreamtale-user-service/src/main/java/com/dreamtale/user/service/service/UserRegisterService.java
@@ -1,11 +1,17 @@
 package com.dreamtale.user.service.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRegisterService {
+    
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     public void register(){
+
     }
 
 }
